6bb189cd5ebdf39923feb8f54cea228a96119ab0
Cyril AKNINE <caknine@clever-age.com>
2020-03-29 16:36:01 +0200

    Preparing autobuild [ci skip]

--- travis-build.sh
+++ travis-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 NPS_VERSION=`curl -s https://api.github.com/repos/apache/incubator-pagespeed-ngx/tags |grep "name" |grep "stable" |head -1 |sed -n "s/^.*v\(.*\)-stable.*$/\1/p"`
-NGINX_VERSION=`curl -s https://nginx.org/packages/ubuntu/dists/xenial/nginx/binary-amd64/Packages.gz |zcat |php -r 'preg_match_all("#Package: nginx\nVersion: (.*?)-\d~.*?\nArch#", file_get_contents("php://stdin"), $m);echo implode($m[1], "\n")."\n";' |sort -r |head -1`
+NGINX_VERSION=`curl -s https://nginx.org/packages/ubuntu/dists/bionic/nginx/binary-amd64/Packages.gz |zcat |php -r 'preg_match_all("#Package: nginx\nVersion: (.*?)-\d~.*?\nArch#", file_get_contents("php://stdin"), $m);echo implode($m[1], "\n")."\n";' |sort -r |head -1`
 
 docker build -t build-nginx-pagespeed -f Dockerfile-deb \
 	--build-arg DISTRIB=${OS} --build-arg RELEASE=${DIST} \
