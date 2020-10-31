3575c40a12c52aa39174c685afa01c3d2c963afd
Cyril AKNINE <caknine@clever-age.com>
2020-10-31 19:25:18 +0100

    Updated upstream & deb version correction.

--- autobump.sh
+++ autobump.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 NPS_VERSION=`curl -s https://api.github.com/repos/apache/incubator-pagespeed-ngx/tags |grep "name" |grep "stable" |head -1 |sed -n "s/^.*v\(.*\)-stable.*$/\1/p"`
-NGINX_VERSION=`curl -s https://nginx.org/packages/ubuntu/dists/bionic/nginx/binary-amd64/Packages.gz |zcat |php -r 'preg_match_all("#Package: nginx\nVersion: (.*?)-\d~.*?\nArch#", file_get_contents("php://stdin"), $m);echo implode($m[1], "\n")."\n";' |sort -r |head -1`
+NGINX_DEB_VERSION=`curl -s https://nginx.org/packages/debian/pool/nginx/n/nginx/ |grep '"nginx_' | sed -n "s/^.*\">nginx_\(.*\)\~.*$/\1/p" |sort -Vr |head -1`
 
 docker build --pull -t deb-dch -f Dockerfile-deb-dch .
 docker run -it -v $PWD:/local -e HOME=/local deb-dch bash -c "cd /local && \
-    dch -M -v ${NPS_VERSION}+nginx-${NGINX_VERSION}-1~stretch --distribution 'stretch' 'Updated upstream.'"
+    dch -M -v ${NPS_VERSION}+nginx-${NGINX_DEB_VERSION}~stretch --distribution 'stretch' 'Updated upstream.'"
 
 git add debian/changelog
 git commit -m "Updated upstream."
-git tag "pagespeed-${NPS_VERSION}/nginx-${NGINX_VERSION}"
+git tag "pagespeed-${NPS_VERSION}/nginx-${NGINX_DEB_VERSION}"
 git push origin --tags
-#git push origin "pagespeed-${NPS_VERSION}/nginx-${NGINX_VERSION}"
+#git push origin "pagespeed-${NPS_VERSION}/nginx-${NGINX_DEB_VERSION}"
